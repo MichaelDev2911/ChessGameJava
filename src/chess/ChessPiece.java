5c54bc172858c8dc2926dc293a6dc7d2794978c6
MichaelDev2911 <michaelsilveira.dev@outlook.com>
2021-04-11 20:36:18 -0300

    Increase and decrease

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,14 +6,27 @@ import boardgame.Position;
 
 public abstract class ChessPiece  extends Piece {
     private Color color;
+    private Integer moveCount;
+
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
     }
 
+    public Integer getMoveCount() {
+        return moveCount;
+    }
+
     public Color getColor() {
         return color;
+    }
+
+    public void increaseMoveCount(){
+        moveCount++;
+    }
+    public void decreaseMoveCount(){
+        moveCount--;
     }
 
     public ChessPosition getChessPosition(){
